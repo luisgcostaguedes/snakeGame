ba38c9b94521316c0392a1eb7274185d57fca249
luisgcostaguedes <luisgcostaguedes@gmail.com>
2023-07-20 15:04:06 -0300

    up food

--- js/script.js
+++ js/script.js
@@ -4,16 +4,29 @@ const ctx = canvas.getContext("2d")
 const size = 20
 
 const snake = [
-  {x:200, y:200},
-  {x:220, y:200},
-  
+  {x:400, y:400}] // cobra vai ser uma array que vai sendo implementada a cada comida que ela come
 
+const food = {
+  x: 100,
+  y: 100,
+  color: "yellow"
 
-] // cobra vai ser uma array que vai sendo implementada a cada comida que ela come
-
+} 
 let direction, loopId;
 
-const drewSnake = () => {
+const drawFood = () => {
+
+  const {x, y, color} = food //exemplo de desestruturação que aprendi no jstack e agora usando em outro projeto
+ // podemos ver que as linhas abaixo não precisam mais usar o ponto alguma coisa que nem era antes (food.x, food.y e assim por diante)
+  ctx.shadowColor = color
+  ctx.shadowBlur = 6
+  ctx.fillStyle = color
+  ctx.fillRect(x, y, size, size)
+  ctx.shadowBlur = 0
+}
+
+
+const drawSnake = () => {
   ctx.fillStyle = "#ddd"
   snake.forEach((position, index) => {
     if(index == snake.length -1){
@@ -51,16 +64,43 @@ const moveSnake = () => {
   snake.shift()
 }
 
+const drawGrid = () => {
+  ctx.lineWidth = 1
+  ctx.strokeStyle = "#191919"
+
+  for (let i = 20; i < canvas.width; i += 20) {
+
+    ctx.beginPath()
+    ctx.lineTo(i, 0)
+    ctx.lineTo(i, 800)
+    ctx.stroke()   
+
+    ctx.beginPath()
+    ctx.lineTo(0, i)
+    ctx.lineTo(800, i)
+    ctx.stroke()   
+
+    
+  }
+
+}
+
 const gameLoop = () => {
   clearInterval(loopId)
   ctx.clearRect(0,0, 800, 800)
+  
+
+  
+  drawGrid();
+  drawFood();
   moveSnake();
-  drewSnake();
+  drawSnake();
+  
 
   loopId = setTimeout(() => {
     gameLoop()
 
-  },100)
+  },300)
 }
 
 gameLoop()
