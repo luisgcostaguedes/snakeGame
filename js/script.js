780a0dcf2f0dacb7b48a7d1f195228e5db46a974
luisgcostaguedes <luisgcostaguedes@gmail.com>
2023-07-24 14:04:48 -0300

    up quase lá

--- js/script.js
+++ js/script.js
@@ -1,15 +1,37 @@
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
-
+const audio = new Audio('../assets/audio.mp3')
 const size = 20
 
 const snake = [
   {x:400, y:400}] // cobra vai ser uma array que vai sendo implementada a cada comida que ela come
 
+const randowNumber = (min, max) => {
+  return Math.round( Math.random() * (max-min)+ min)
+}
+
+const randowPosition = () => {
+  const number = randowNumber(0, canvas.width - size) // aqui pegamos o tamanho de toda a área do jogo e tiramos tamanho da comida, para que ela nunca caia na bairada do ambiente do jogo
+  return Math.round(number/20) * 20 //para tornar os numeros aleatórios utilizei esta formula para que seja gerado multiplos de 20, que é o tamanho dos espaços do jogo
+}
+
+const randomColor = () => {
+  const red = randowNumber(0,255)
+  const green = randowNumber(0,255)
+  const blue = randowNumber(0,255)
+
+  return `rgb(${red}, ${green}, ${blue})` //utilizando a função de numero aleatórios podemos criar 3 variaveis que vão formar as cores aleatórias para as comidas do jogo
+
+}
+
 const food = {
-  x: 100,
-  y: 100,
-  color: "yellow"
+  x: randowPosition(),
+  y: randowPosition(),
+  color: randomColor(),
+
+  // aqui utilizamos o numero aleatorio para gerar as coordenadas de x e y, para onde a comida vai ficar
+  // considerando a media retirada do size, para que não caia número perto de 800 que é o limite da bairada do jogo
+
 
 } 
 let direction, loopId;
@@ -82,6 +104,39 @@ const drawGrid = () => {
 
     
   }
+//função utilizada para a criação do grid do jogo onde desenhamos as linhas em x e y
+}
+
+const checkEat = () => {
+  const head = snake.at(-1)
+
+  if(head.x == food.x && head.y == food.y){
+    snake.push(head)
+    audio.play()
+
+    let x = randowPosition()
+    let y = randowPosition()
+
+    while (snake.find((position) => position.x == x && position.y == y )){     x = randowPosition(),
+      y = randowPosition()
+    }
+
+    food.x = x
+    food.y = y
+    food.color = randomColor()
+
+  }
+
+}
+
+const checkCollision = () => {
+  const head = snake[snake.length - 1]
+
+  if (head.x < 0 || head.x > 780 || head.y < 0 || head.y > 780) {
+
+    alert("Você perdeu!!")
+  }
+
 
 }
 
@@ -95,12 +150,13 @@ const gameLoop = () => {
   drawFood();
   moveSnake();
   drawSnake();
+  checkEat();
   
 
   loopId = setTimeout(() => {
     gameLoop()
 
-  },300)
+  },100)
 }
 
 gameLoop()
@@ -122,7 +178,7 @@ document.addEventListener("keydown", ({key}) => {
     direction = "down"
   }
 
-
+// função para pegar os botões que são pressionados 
 
 })
 
